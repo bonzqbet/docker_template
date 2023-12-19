72c04264a3628af0d8300862ca2905ea167f1e2c
Phanuwat Sutthiwong <playgroundclub_1@hotmail.com>
2023-12-19 16:56:34 +0700

    update

---
+++ php_5/volumes/src/connect.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'db';
+$user = 'root';
+$pass = 'MYSQL_ROOT_PASSWORD';
+$db = 'sys';
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
